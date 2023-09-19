0dbf569acc56ec134fa031291d7ed0dad2c0380b
shahan12 <shahankhan485@gmail.com>
2023-09-19 12:16:48 +0530

    added redux

---
+++ src/components/requirementHeader/requirementHeader.compoennt.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.css";
+
+function Header(props) {
+  return <div className="header-component-wrapper"></div>;
+}
+
+export default Header;
---
+++ src/pages/order/orders.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./orders.css";
+import Header from "../../components/requirementHeader/requirementHeader.compoennt";
+
+function Order(props) {
+  return (
+    <div className="orders-compoennt-wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default Order;
