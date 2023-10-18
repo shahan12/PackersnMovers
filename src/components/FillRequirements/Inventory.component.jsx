591548ea84969c752fffef802d36dd8ec496ec35
shahan12 <shahankhan485@gmail.com>
2023-10-19 00:36:40 +0530

    select date and time fix

--- src/components/FillRequirements/Inventory.component.jsx
+++ src/components/FillRequirements/Inventory.component.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
-import InventoryItem from './InventoryItem.component';
-import FURNITURE from '../DATA/FinalCFT.json';
-import minus from '../../images/minus.png';
-import plus from '../../images/plus.png';
-import itembox from '../../images/itembox.png';
-import Uparrow from '../../images/upArrowinventory.png';
-import downarrow from '../../images/downArrowinventory.png';
+import React, { useState } from "react";
+import InventoryItem from "./InventoryItem.component";
+import FURNITURE from "../DATA/FinalCFT.json";
+import minus from "../../images/minus.png";
+import plus from "../../images/plus.png";
+import itembox from "../../images/itembox.png";
+import Uparrow from "../../images/upArrowinventory.png";
+import downarrow from "../../images/downArrowinventory.png";
 import "./Inventory.css";
-import { useDispatch } from 'react-redux';
-import { updateSelectedItems } from '../../redux/actions';
+import { useDispatch } from "react-redux";
+import { updateSelectedItems } from "../../redux/actions";
 
-const Inventory = ({progress, setProgress}) => {
-
+const Inventory = ({ progress, setProgress }) => {
   const dispatch = useDispatch();
   const [itemCount, setItemCount] = useState(0);
   const [inventoryData, setInventoryData] = useState(FURNITURE);
@@ -19,16 +18,18 @@ const Inventory = ({progress, setProgress}) => {
   const [selectedItems, setSelectedItems] = useState({});
   const [selectedMaterialMap, setSelectedMaterialMap] = useState({});
   const [selectedTypeMap, setSelectedTypeMap] = useState({});
-  const [selectedCategory, setSelectedCategory] = useState(Object.keys(inventoryData)[0]);
+  const [selectedCategory, setSelectedCategory] = useState(
+    Object.keys(inventoryData)[0]
+  );
   const handleItemClick = (item) => {
     setExpandedItem(item);
   };
 
   const FlatrequireMents = () => {
-    if (progress === 'inventory') {
-      setProgress('dateselection');
-    }}
-
+    if (progress === "inventory") {
+      setProgress("dateselection");
+    }
+  };
 
   const handleAddVariation = (category, subItem, name) => {
     const itemToDuplicate = inventoryData[category][subItem][name];
@@ -39,14 +40,14 @@ const Inventory = ({progress, setProgress}) => {
       duplicatedName = `${name} - variation ${variationCount}`;
     }
     inventoryData[category][subItem][duplicatedName] = { ...itemToDuplicate };
-    setInventoryData({ ...inventoryData });  
+    setInventoryData({ ...inventoryData });
   };
 
   const handlePlusClick = (name, typeMap, materialMap, category, subItem) => {
     if (!typeMap & !materialMap) {
       return;
     }
-  
+
     const count = selectedItems[category][subItem][name].count;
     selectedItems[category][subItem][name].count = count + 1;
     setSelectedItems({ ...selectedItems });
@@ -56,10 +57,10 @@ const Inventory = ({progress, setProgress}) => {
   //   if (!typeMap || !materialMap) {
   //     return;
   //   }
-  
+
   //   setSelectedItems((prevSelectedItems) => {
   //     const updatedSelectedItems = { ...prevSelectedItems };
-  
+
   //     if (!updatedSelectedItems[category]) {
   //       updatedSelectedItems[category] = {};
   //     }
@@ -75,17 +76,17 @@ const Inventory = ({progress, setProgress}) => {
   //       const count = updatedSelectedItems[category][subItem][name].count || 0;
   //       updatedSelectedItems[category][subItem][name].count = count + 1;
   //     }
-  
+
   //     dispatch(updateSelectedItems(updatedSelectedItems));
-  
+
   //     return updatedSelectedItems;
   //   });
   // };
-  
+
   const handleTypeChange = (name, category, subItem, type, material) => {
-    setSelectedTypeMap(prevTypeMap => ({
+    setSelectedTypeMap((prevTypeMap) => ({
       ...prevTypeMap,
-      [name]: type
+      [name]: type,
     }));
     const updatedItems = { ...selectedItems };
     if (!updatedItems[category]) {
@@ -97,19 +98,28 @@ const Inventory = ({progress, setProgress}) => {
     if (!updatedItems[category][subItem][name]) {
       updatedItems[category][subItem][name] = {};
     }
-  
-    updatedItems[category][subItem][name].type = type;
-    updatedItems[category][subItem][name].material = selectedMaterialMap[name] || Object.keys(inventoryData[category][subItem][name].material)[0]; updatedItems[category][subItem][name].cost = calculateCost(name, category, subItem, type, updatedItems[category][subItem][name].material);
-    updatedItems[category][subItem][name].count = updatedItems[category][subItem][name].count || 0;
 
-  
+    updatedItems[category][subItem][name].type = type;
+    updatedItems[category][subItem][name].material =
+      selectedMaterialMap[name] ||
+      Object.keys(inventoryData[category][subItem][name].material)[0];
+    updatedItems[category][subItem][name].cost = calculateCost(
+      name,
+      category,
+      subItem,
+      type,
+      updatedItems[category][subItem][name].material
+    );
+    updatedItems[category][subItem][name].count =
+      updatedItems[category][subItem][name].count || 0;
+
     setSelectedItems(updatedItems);
   };
-  
+
   const handleMaterialChange = (name, category, subItem, material, type) => {
-    setSelectedMaterialMap(prevMaterialMap => ({
+    setSelectedMaterialMap((prevMaterialMap) => ({
       ...prevMaterialMap,
-      [name]: material
+      [name]: material,
     }));
     const updatedItems = { ...selectedItems };
     if (!updatedItems[category]) {
@@ -122,15 +132,26 @@ const Inventory = ({progress, setProgress}) => {
       updatedItems[category][subItem][name] = {};
     }
     updatedItems[category][subItem][name].material = material;
-    updatedItems[category][subItem][name].type = selectedTypeMap[name] || Object.keys(inventoryData[category][subItem][name].type)[0]; updatedItems[category][subItem][name].cost = calculateCost(name, category, subItem, type, updatedItems[category][subItem][name].material);
-    updatedItems[category][subItem][name].count = updatedItems[category][subItem][name].count || 0;
+    updatedItems[category][subItem][name].type =
+      selectedTypeMap[name] ||
+      Object.keys(inventoryData[category][subItem][name].type)[0];
+    updatedItems[category][subItem][name].cost = calculateCost(
+      name,
+      category,
+      subItem,
+      type,
+      updatedItems[category][subItem][name].material
+    );
+    updatedItems[category][subItem][name].count =
+      updatedItems[category][subItem][name].count || 0;
 
     setSelectedItems(updatedItems);
   };
 
   const calculateCost = (name, category, subItem, type, material) => {
     const base = inventoryData[category][subItem][name].base;
-    const materialValue = inventoryData[category][subItem][name].material[material];
+    const materialValue =
+      inventoryData[category][subItem][name].material[material];
     const typeValue = inventoryData[category][subItem][name].type[type];
     return base + materialValue + typeValue;
   };
@@ -138,7 +159,8 @@ const Inventory = ({progress, setProgress}) => {
   const handleMinusClick = (name, category, subItem) => {
     setSelectedItems((prevSelectedItems) => {
       const updatedSelectedItems = { ...prevSelectedItems };
-      const currentCount = updatedSelectedItems[category]?.[subItem]?.[name]?.count || 0;
+      const currentCount =
+        updatedSelectedItems[category]?.[subItem]?.[name]?.count || 0;
       if (currentCount > 0) {
         if (!updatedSelectedItems[category]) {
           updatedSelectedItems[category] = {};
@@ -154,61 +176,102 @@ const Inventory = ({progress, setProgress}) => {
       dispatch(updateSelectedItems(selectedItems));
       return updatedSelectedItems;
     });
-    
   };
   const renderSubItems = (subItems, category) => {
     return Object.keys(subItems).map((subItem) => (
-      <div className="category-content-p" onClick={() => handleItemClick(subItem)} key={subItem}>
+      <div
+        className="category-content-p"
+        onClick={() => handleItemClick(subItem)}
+        key={subItem}
+      >
         <div className="category-content-p-holder">
           {subItem}
-          <img style={{marginRight: '0.5rem'}} src={expandedItem === subItem ? Uparrow :downarrow} />
+          <img
+            style={{ marginRight: "0.5rem" }}
+            src={expandedItem === subItem ? Uparrow : downarrow}
+          />
         </div>
         {expandedItem === subItem && (
-          <div>
-            {renderItemDetails(subItems[subItem], subItem, category)}
-          </div>
+          <div>{renderItemDetails(subItems[subItem], subItem, category)}</div>
         )}
       </div>
     ));
   };
 
-  const renderItemDetails = (data, subItem, category) => { // item box\
+  const renderItemDetails = (data, subItem, category) => {
+    // item box\
     return (
-      <div className='itemDetails-parent'>
-      {Object.keys(data).map((name, index) => (
-        <div className='itemDetails' key={index}>
-          <span>{name}</span>
-          <select
-            className='custom-select'
-            onChange={(e) => handleMaterialChange(name, category, subItem, e.target.value, selectedTypeMap[name] || Object.keys(data[name].type)[0])}
-            value={selectedMaterialMap[name] || selectedMaterialMap[Object.keys(data[name].material)[0]]} // Set the initial value to the first material option
-          >
-            <option value=''>Select Attribute</option>
-            {Object.keys(data[name].material).map((material, materialIndex) => (
-              <option key={materialIndex} value={material}>
-                {material}
-              </option>
-            ))}
-          </select>
-          <select
-            className='custom-select'
-            onChange={(e) => handleTypeChange(name, category, subItem, e.target.value, selectedMaterialMap[name] || Object.keys(data[name].material)[0])}
-            value={selectedTypeMap[name] || selectedTypeMap[Object.keys(data[name].type)[0]]}
-          >
-            <option value=''>Select Type</option>
-            {Object.keys(data[name].type).map((type, typeIndex) => (
-              <option key={typeIndex} value={type}>
-                {type}
-              </option>
-            ))}
-          </select>
-          <div className='itemDetails-child'>
-              <div className='itemDetails-child-inc'>
-                <img src={minus} onClick={() => handleMinusClick(name, category, subItem)} />
-                <span>{selectedItems[category]?.[subItem]?.[name]?.count || 0}</span>
+      <div className="itemDetails-parent">
+        {Object.keys(data).map((name, index) => (
+          <div className="itemDetails" key={index}>
+            <span>{name}</span>
+            <select
+              className="custom-select"
+              onChange={(e) =>
+                handleMaterialChange(
+                  name,
+                  category,
+                  subItem,
+                  e.target.value,
+                  selectedTypeMap[name] || Object.keys(data[name]?.type)[0]
+                )
+              }
+              value={
+                selectedMaterialMap[name] ||
+                selectedMaterialMap[Object.keys(data[name]?.material)[0]]
+              } // Set the initial value to the first material option
+            >
+              <option value="">Select Attribute</option>
+              {Object.keys(data[name].material).map(
+                (material, materialIndex) => (
+                  <option key={materialIndex} value={material}>
+                    {material}
+                  </option>
+                )
+              )}
+            </select>
+            <select
+              className="custom-select"
+              onChange={(e) =>
+                handleTypeChange(
+                  name,
+                  category,
+                  subItem,
+                  e.target.value,
+                  selectedMaterialMap[name] ||
+                    Object.keys(data[name]?.material)[0]
+                )
+              }
+              value={
+                selectedTypeMap[name] ||
+                selectedTypeMap[Object.keys(data[name]?.type)[0]]
+              }
+            >
+              <option value="">Select Type</option>
+              {Object.keys(data[name].type).map((type, typeIndex) => (
+                <option key={typeIndex} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+            <div className="itemDetails-child">
+              <div className="itemDetails-child-inc">
+                <img
+                  src={minus}
+                  onClick={() => handleMinusClick(name, category, subItem)}
+                />
+                <span>
+                  {selectedItems[category]?.[subItem]?.[name]?.count || 0}
+                </span>
                 <button
                   onClick={() =>
-                    handlePlusClick(name, selectedTypeMap[name], selectedMaterialMap[name], category, subItem)
+                    handlePlusClick(
+                      name,
+                      selectedTypeMap[name],
+                      selectedMaterialMap[name],
+                      category,
+                      subItem
+                    )
                   }
                   disabled={
                     !selectedTypeMap[name] ||
@@ -219,15 +282,19 @@ const Inventory = ({progress, setProgress}) => {
                   <img src={plus} alt="Add" />
                 </button>
               </div>
-              {name.includes('variation') ? (
-                ''
+              {name.includes("variation") ? (
+                ""
               ) : (
-                <span onClick={() => handleAddVariation(category, subItem, name)}>Add Variation</span>
+                <span
+                  onClick={() => handleAddVariation(category, subItem, name)}
+                >
+                  Add Variation
+                </span>
               )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     );
   };
   return (
@@ -235,20 +302,24 @@ const Inventory = ({progress, setProgress}) => {
       <div className="border-bottom extra-margin">
         <h2>Fill Requirements</h2>
       </div>
-      <div className='ItemAdded'>
-        <div className='ItemAddedChild'>
-          <img style={{marginRight: '0.5rem'}} src={itembox} />
+      <div className="ItemAdded">
+        <div className="ItemAddedChild">
+          <img style={{ marginRight: "0.5rem" }} src={itembox} />
           <span>Total Items Added</span>
         </div>
         {itemCount}
       </div>
-      <div className='Inventory-container'>
+      <div className="Inventory-container">
         <span>Add Items from Inventory</span>
-        <div className='inventory-selection-parent'>
+        <div className="inventory-selection-parent">
           {Object.keys(inventoryData).map((category) => (
             <span
               key={category}
-              className={category === selectedCategory ? 'selected-inventory' : 'non-selected-inventory'}
+              className={
+                category === selectedCategory
+                  ? "selected-inventory"
+                  : "non-selected-inventory"
+              }
               onClick={() => setSelectedCategory(category)}
             >
               {category}
@@ -262,10 +333,12 @@ const Inventory = ({progress, setProgress}) => {
             {renderSubItems(inventoryData[selectedCategory], selectedCategory)}
           </div>
         </div>
-    </div>
-    
-      < div className="fill-req-CTA-container flex">
-        <button className="cta-button" onClick={FlatrequireMents}>NEXT</button>
+      </div>
+
+      <div className="fill-req-CTA-container flex">
+        <button className="cta-button" onClick={FlatrequireMents}>
+          NEXT
+        </button>
       </div>
     </div>
   );
--- src/components/OptionsSelect/dateOption.component.jsx
+++ src/components/OptionsSelect/dateOption.component.jsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
-import './options.css';
-import CalendarModal from './CalendarModel.component';
+import React, { useState } from "react";
+import "./options.css";
+import CalendarModal from "./CalendarModel.component";
 
 function DateOption({ onSelect, value }) {
   const [selectedDay, setSelectedDay] = useState(null);
 
-  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   const generateDays = () => {
     const today = new Date();
     const days = [];
-  
+
     for (let i = 0; i < 15; i++) {
       const currentDate = new Date(today);
       currentDate.setDate(today.getDate() + i);
-  
+
       const dayOfWeek = weekdays[currentDate.getDay()];
-      const date = currentDate.getDate(); 
-  
+      const date = currentDate.getDate();
+
       let price = 2499;
       let isWeekend = false;
-  
+
       if (currentDate.getDay() === 0 || currentDate.getDay() === 6) {
         // Weekend (Sunday or Saturday)
         price = price * 1.2;
         isWeekend = true;
       }
-  
+
       days.push({ dayOfWeek, date, price, isWeekend, currentDate });
     }
-  
+
     return days;
   };
-  
+
   const generatedData = generateDays();
 
   const handleDayClick = (day) => {
@@ -54,31 +62,39 @@ function DateOption({ onSelect, value }) {
   const closeModal = () => {
     setModalOpen(false);
   };
-
+  console.log(generatedData, "genData");
+  console.log(selectedDay, "selectedDat");
   return (
-  <div className="day-box">
-    {generatedData.map((day, index) => (
-      <div
-        key={index}
-        className={`daybox-item ${selectedDay === day ? 'selected' : ''}`}
-        onClick={(e) => {
-          e.stopPropagation();
-          e.preventDefault();
-          handleDayClick(day);
-        }}
-      >
-        <p >{index === 0 ? 'Tomorrow' : day.dayOfWeek}</p>
-        <p className='dayofweek'> {day.date}</p>
-        <p className={day.isWeekend ? 'weekend-price' : 'weekday-price'}>₹{day.price.toFixed(0)}</p>
-      </div>
-    ))}
-{/* 
+    <div className="day-box">
+      {generatedData.map((day, index) => (
+        <div
+          key={index}
+          className={`daybox-item ${
+            selectedDay &&
+            selectedDay.date === day.date &&
+            selectedDay.dayOfWeek === day.dayOfWeek
+              ? "selected"
+              : ""
+          }`}
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            handleDayClick(day);
+          }}
+        >
+          <p>{index === 0 ? "Tomorrow" : day.dayOfWeek}</p>
+          <p className="dayofweek"> {day.date}</p>
+          <p className={day.isWeekend ? "weekend-price" : "weekday-price"}>
+            ₹{day.price.toFixed(0)}
+          </p>
+        </div>
+      ))}
+      {/* 
     <div onClick={openModal} className="daybox-item">
       <p>Calendar</p>
     </div>
     <CalendarModal isOpen={isModalOpen} onRequestClose={closeModal} date={date1} onDateChange={onDateChange} /> */}
-  </div>
-
+    </div>
   );
 }
 
