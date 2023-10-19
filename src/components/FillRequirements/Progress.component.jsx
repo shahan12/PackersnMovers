3a06b2ad367f46a7a23ad152d33a2dea932fd248
shahan12 <shahankhan485@gmail.com>
2023-10-20 01:13:50 +0530

    added popUp for thanks + nav bar changes

--- src/components/FillRequirements/Progress.component.jsx
+++ src/components/FillRequirements/Progress.component.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import "./calendar.css";
 
-import Modal from 'react-modal';
-const Progress = ({progress, setProgress}) => {
-
+import Modal from "react-modal";
+import ThankYouModal from "../ThankYouModal/thankYouModal.component";
+const Progress = ({ progress, setProgress }) => {
   let ITEMADDED = useSelector((state) => state.selectedItems);
   console.log("ITEMADDED", ITEMADDED);
   const prev = () => {
-    if (progress === 'progress') {
-      setProgress('dateselection');
-  }};
+    if (progress === "progress") {
+      setProgress("dateselection");
+    }
+  };
 
   let DateTime = useSelector((state) => state.DateTime);
-  
+
   const bookingConfirm = () => {
-    if (progress === 'progress') {
+    if (progress === "progress") {
       openModal();
-    }};
+    }
+  };
 
   let totalCost = useSelector((state) => state.TotalCostItems);
 
@@ -37,55 +39,55 @@ const Progress = ({progress, setProgress}) => {
         <h2>Confirm</h2>
       </div>
       <div className="cost-details">
-          <div className="cost-details-child"> 
-            <span style={{fontWeight: '600', fontSize: '1.2rem'}}>Order Details</span>
-          </div>
-          <div className="cost-details-child"> 
-            <span>Base Price</span>
-            <span>{totalCost.basePrice}</span>
-          </div>
-          <div className="cost-details-child"> 
-            <span>Floor Charges</span>
-            <span>{totalCost.floorCharges}</span>
-          </div>
-          <div className="cost-details-child"> 
-            <span>Total Items Added</span>
-            <span>{totalCost.totalItemCount}</span>
-          </div>
-          <div className="cost-details-child"> 
-            <span>CFT</span>
-            <span>{totalCost.cft}</span>
-          </div>
-          <div className="cost-details-child"> 
-            <span>Add Ons</span>
-            <span>{totalCost.addonsPrice}</span>
-          </div>
-          <div className="cost-details-child"> 
-            <span>Packaging Selected: {totalCost.packaging}</span>
-            <span>{totalCost.packagingPrice}</span>
-          </div>
-          <div className="cost-details-child cost-line"> 
-            <span>Total Cost: </span>
-            <span className="highlightcost">₹{totalCost.totalCost}</span>
-          </div>
+        <div className="cost-details-child">
+          <span style={{ fontWeight: "600", fontSize: "1.2rem" }}>
+            Order Details
+          </span>
+        </div>
+        <div className="cost-details-child">
+          <span>Base Price</span>
+          <span>{totalCost.basePrice}</span>
+        </div>
+        <div className="cost-details-child">
+          <span>Floor Charges</span>
+          <span>{totalCost.floorCharges}</span>
+        </div>
+        <div className="cost-details-child">
+          <span>Total Items Added</span>
+          <span>{totalCost.totalItemCount}</span>
+        </div>
+        <div className="cost-details-child">
+          <span>CFT</span>
+          <span>{totalCost.cft}</span>
+        </div>
+        <div className="cost-details-child">
+          <span>Add Ons</span>
+          <span>{totalCost.addonsPrice}</span>
+        </div>
+        <div className="cost-details-child">
+          <span>Packaging Selected: {totalCost.packaging}</span>
+          <span>{totalCost.packagingPrice}</span>
+        </div>
+        <div className="cost-details-child cost-line">
+          <span>Total Cost: </span>
+          <span className="highlightcost">₹{totalCost.totalCost}</span>
+        </div>
       </div>
-      
+
       <div className="fill-req-CTA-container flex nextbuttonMove">
-        <div className='prevButton' onClick={prev}>&lt; Previous</div>
-        <button className="cta-button" onClick={bookingConfirm}>Confirm Booking</button>
-      </div>
-      <Modal
-        className="thankyou"
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="Thank You Modal"
-      >
-        <h2>Thank You</h2>
-        <p>Your booking has been confirmed. We appreciate your business!</p>
-        <button className="cta-button" onClick={closeModal}>
-          Close
+        <div className="prevButton" onClick={prev}>
+          &lt; Previous
+        </div>
+        <button className="cta-button" onClick={bookingConfirm}>
+          Confirm Booking
         </button>
-      </Modal>
+      </div>
+      {isModalOpen && (
+        <ThankYouModal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setModalOpen}
+        />
+      )}
     </div>
   );
 };
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,20 @@ import { Link } from "react-router-dom";
 import "./header.css";
 import logo from "../../images/SHIFTKART-LOGO.png";
 import RegisterModal from "../RegisterModal/RegisterModal.component";
+import PhoneIcon from "../../images/phone.svg";
+import ProfilePic from "../../images/defaultPic.svg";
+import DownArrow from "../../images/downarrow.png";
 
 function Header({ showPopUp, isAuthenticated, loginModal, setLoginModal }) {
+  const [showfillHeader, setShowFillHeader] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+  const [showMoreOption, setShowMoreOption] = useState(false);
+
+  useEffect(() => {
+    if (window.location.pathname.includes("fill-details") > 0) {
+      setShowFillHeader(true);
+    }
+  }, [window.location.pathname]);
 
   const closeModal = () => {
     setModalOpen(false);
@@ -35,18 +46,46 @@ function Header({ showPopUp, isAuthenticated, loginModal, setLoginModal }) {
       <div className="align-center">
         <img src={logo} alt="logo" className="header-logo-img"></img>
       </div>
-      <div className="header-cta-container align-center space-between grey-600">
-        <div className="header-sign-in-btn">
-          <Link to="/" className="header-CTA-item">
-            Home
-          </Link>
+      {showfillHeader ? (
+        <div className="header-cta-container align-center space-between grey-600">
+          <span className="header-item-phoneNumber-container">
+            <img src={PhoneIcon} alt={"phone"}></img>
+            <span>+91 884788</span>
+          </span>
+          <div className="header-profile-picture-container">
+            <img
+              src={ProfilePic}
+              alt="profile-pic"
+              className="flex width-100"
+            ></img>
+          </div>
+          <div
+            className="header-more-option-container"
+            onClick={() => setShowMoreOption(!showMoreOption)}
+          >
+            <img
+              src={DownArrow}
+              alt="down-Arrow"
+              className="flex width-100 header-profile-img"
+            ></img>
+          </div>
+          {showMoreOption && (
+            <div className="header-more-option-dropdown">Logout</div>
+          )}
         </div>
-        <div className="header-sign-in-btn">
-          <Link to="/about-us" className="header-CTA-item">
-            About Us
-          </Link>
-        </div>
-        {/* <div className="header-user-wrapper">
+      ) : (
+        <div className="header-cta-container align-center space-between grey-600">
+          <div className="header-sign-in-btn">
+            <Link to="/" className="header-CTA-item">
+              Home
+            </Link>
+          </div>
+          <div className="header-sign-in-btn">
+            <Link to="/about-us" className="header-CTA-item">
+              About Us
+            </Link>
+          </div>
+          {/* <div className="header-user-wrapper">
           <button
             className="header-user-wrapper-btn"
             onClick={() => setModalOpen(true)}
@@ -61,21 +100,22 @@ function Header({ showPopUp, isAuthenticated, loginModal, setLoginModal }) {
             />
           )}
         </div> */}
-        {!isAuthenticated ? (
-          <div
-            className="header-sign-in-btn"
-            onClick={() => {
-              setLoginModal(true);
-            }}
-          >
-            Login
-          </div>
-        ) : (
-          <div className="header-user-wrapper" onClick={hanleLogOut}>
-            <span>Log Out</span>
-          </div>
-        )}
-      </div>
+          {!isAuthenticated ? (
+            <div
+              className="header-sign-in-btn"
+              onClick={() => {
+                setLoginModal(true);
+              }}
+            >
+              Login
+            </div>
+          ) : (
+            <div className="header-user-wrapper" onClick={hanleLogOut}>
+              <span>Log Out</span>
+            </div>
+          )}
+        </div>
+      )}
     </article>
   );
 }
