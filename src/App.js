6f98d4fb56fe3b8b5e68bc3598e0764d87c56024
Arun <arunpanigrahi18@gmail.com>
2023-10-20 14:03:55 +0530

    relocate address preserve and check login user

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
               showPopUp={showPopUp}
               setShowPopUp={setShowPopUp}
               loginModal={loginModal}
+              setLoginModal={setLoginModal}
             />
           }
         />
