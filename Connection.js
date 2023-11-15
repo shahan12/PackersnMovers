75d4e3855c9868e27774869ebfb97a1c1b00afcf
obaidr76 <obaidr76@gmail.com>
2023-11-15 16:20:00 +0530

    db

--- Connection.js
+++ Connection.js
@@ -1,10 +1,10 @@
 var pg  = require("pg").Pool;
 
 var con = new pg({
-    host: 'localhost',
+    host: '178.16.139.203',
     user: 'postgres',
-    password: 'root',
-    database: 'root',
+    password: 'Shift@987',
+    database: 'shiftkartdb',
     port:5432
 });
 
