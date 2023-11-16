55fa1278a12ad1b66c2556ad2f6798e0737582e9
obaidr76 <obaidr76@gmail.com>
2023-11-16 19:08:47 +0530

    newupd3

--- src/API/apicalls.js
+++ src/API/apicalls.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // const backendURL = 'https://www.shiftkart.co:3001';
 
 const instance = axios.create({
-  baseURL: 'https://skbootstrap.cloud/api', 
+  baseURL: 'https://shiftkart.co/api', 
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
