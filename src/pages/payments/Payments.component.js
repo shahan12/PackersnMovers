a80eea709303cfcaefc0f6a65bb94499ab57d164
maliknajeeb113 <maliknajeeb113@gmail.com>
2023-12-06 04:09:58 +0530

    Fixed home, about, payments

--- src/pages/payments/Payments.component.js
+++ src/pages/payments/Payments.component.js
@@ -43,9 +43,9 @@ function Payments(props) {
   return (
     <>
       <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }} className="edit-profile-wrapper">
-        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', }}  className="container">
-          <h2>Redirecting in a moment</h2>
-          <img style={{width: '1.5rem', marginRight: '0.5rem'}} src={loaderIcon} alt="loader" />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}  className="container">
+          <h2>Verifying your transaction, please do not refresh your page</h2>
+          <img style={{width: '2.5rem'}} src={loaderIcon} alt="loader" />
         </div>
       </div>
     </>
