978d185ba90ddb7c65adaecb90f1222596338038
maliknajeeb113 <maliknajeeb113@gmail.com>
2023-12-05 18:15:58 +0530

    Auth

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(
     () => JSON.parse(sessionStorage.getItem("loggedIn")) || false
     );
-  const [isAuthenticated, setIsAuthenticated] = useState(() => sessionStorage.getItem("loggedIn") && sessionStorage.getItem("loggedIn") || false);
+  // const [isAuthenticated, setIsAuthenticated] = useState(() => sessionStorage.getItem("loggedIn") && sessionStorage.getItem("loggedIn") || false);
 
   const [loginModal, setLoginModal] = useState(false);
   const [showPopUp, setShowPopUp] = useState(false);
